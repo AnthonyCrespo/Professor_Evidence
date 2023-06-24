c40fc1d6336722535132d01a1964c4e2abfcdb9f
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-06-24 18:19:33 -0500

    Added content for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,95 +18,98 @@ export function HomePage(){
     return (
 
         <div>
-
             {/* ------------  navbar -------------  */}
             <Navbar className="SAED-navbar">
-            <Container>
-                <Navbar.Brand /* href="/home" */>
-                    <img
-                    src="/logo.png"
-                    alt="logo"
-                    width={230}
-                    />  
-                </Navbar.Brand>
-            {/* <Nav className="ms-auto">
-                <Nav.Link  className="navbar-text" href="#home"> USUARIO APELLIDO</Nav.Link>
-                </Nav> */}
+                <Container className="d-flex justify-content-between align-items-center">
+                    <Navbar.Brand className="me-auto">
+                        <img src="/logo.png" alt="logo" width={230} />
+                    </Navbar.Brand>
 
-            <NavDropdown title="NOMBRE APELLIDO" id="evidencias-dropdown">
-                    <NavDropdown.Item href="/login"> Cerrar Sesion</NavDropdown.Item>
-            </NavDropdown>
-            </Container>
+                    <Nav>
+                    <NavDropdown title="NOMBRE APELLIDO" id="evidencias-dropdown">
+                        <NavDropdown.Item href="/login">Cerrar Sesion</NavDropdown.Item>
+                    </NavDropdown>
+                    </Nav>
+                </Container>
             </Navbar>
+
 
             {/* -----------  navbar end -------------  */}
 
 
 
-        {/*------------- sidebar -------------*/}
-        <div className='row'>
-            <div className="col-auto min-vh-100" style={{backgroundColor:"RGB(39, 39, 39)"}}>
-                <ul className="nav flex-column mt-3">
+            {/*------------- sidebar -------------*/}
+            <div className='row'>
+                <div className="col-auto min-vh-100" style={{backgroundColor:"RGB(39, 39, 39)"}}>
+                    <ul className="nav flex-column mt-3">
 
-                {/* -------Home option ------- */}
-                <li className="nav-item mt-2">
-                    <a className="nav-link px-2" href="/home">
-                    <i className='bi bi-house side-bar-icon'></i>
-                    <span className='ms-1 d-none d-sm-inline  side-bar-text'>
-                        Home
-                    </span>
-                    </a>
-                </li>
-                
-                {/* -------Evidencias option ------- */}
-
-                <li className="nav-item mt-2">
-                    <a className="nav-link px-2">
-                    <i className='bi bi-file-text side-bar-icon '></i>
-                    <span className='ms-1 d-none d-sm-inline side-bar-text'>
-                        Evidencias
-                    </span>
-                    </a>
-                    <ul className="nav flex-column ml-3">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/evidencia1" style={{color:"white"}}>
-                        Registrar Evidencia
+                    {/* -------Home option ------- */}
+                    <li className="nav-item mt-2">
+                        <a className="nav-link px-2" href="/home">
+                        <i className='bi bi-house side-bar-icon'></i>
+                        <span className='ms-1 d-none d-sm-inline  side-bar-text'>
+                            Home
+                        </span>
                         </a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link " href="/evidencia2" style={{color:"white"}}>
-                        Evidencia Registrada
+                    
+                    {/* -------Evidencias option ------- */}
+
+                    <li className="nav-item mt-2">
+                        <a className="nav-link px-2">
+                        <i className='bi bi-file-text side-bar-icon '></i>
+                        <span className='ms-1 d-none d-sm-inline side-bar-text'>
+                            Evidencias
+                        </span>
                         </a>
+                        <ul className="nav flex-column ml-3">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/evidencia1" style={{color:"white"}}>
+                            Registrar Evidencia
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link " href="/evidencia2" style={{color:"white"}}>
+                            Evidencia Registrada
+                            </a>
+                        </li>
+                        </ul>
+                    </li>
+
+                    {/* -------Informes option ------- */}
+                    <li className="nav-item mt-2">
+                        <a className="nav-link px-2" href="/">
+                        <i className='bi bi-file-richtext side-bar-icon'></i>
+                        <span className='ms-1 d-none d-sm-inline  side-bar-text'>
+                            Informes
+                        </span>
+                        </a>
+                        <ul className="nav flex-column ml-3">
+                        <li className="nav-item  side-bar-text">
+                            <a className="nav-link" href="/informe1" style={{color:"white"}}>
+                            Crear Informe
+                            </a>
+                        </li>
+                        <li className="nav-item  side-bar-text">
+                            <a className="nav-link" href="/informe2" style={{color:"white"}}>
+                            Observar Informe
+                            </a>
+                        </li>
+                        </ul>
                     </li>
                     </ul>
-                </li>
-
-                {/* -------Informes option ------- */}
-                <li className="nav-item mt-2">
-                    <a className="nav-link px-2" href="/">
-                    <i className='bi bi-file-richtext side-bar-icon'></i>
-                    <span className='ms-1 d-none d-sm-inline  side-bar-text'>
-                        Informes
-                    </span>
-                    </a>
-                    <ul className="nav flex-column ml-3">
-                    <li className="nav-item  side-bar-text">
-                        <a className="nav-link" href="/informe1" style={{color:"white"}}>
-                        Crear Informe
-                        </a>
-                    </li>
-                    <li className="nav-item  side-bar-text">
-                        <a className="nav-link" href="/informe2" style={{color:"white"}}>
-                        Observar Informe
-                        </a>
-                    </li>
-                    </ul>
-                </li>
-                </ul>
+                </div>
+                {/*------------- sidebar ends -------------*/}
+                <div className="col d-flex flex-column align-items-center mt-3">
+                    <h3 className="text-center mb-3" style={{ marginTop: '30px' }}>
+                        Universidad de Tecnología Experimental Yachay Tech
+                    </h3>
+                    <img src="/logo_saed.png" className="img-fluid" alt="Imagen centrada"
+                    style={{ width: '550px', height: 'auto' }} />
+                </div>
             </div>
+            
         </div>
-        {/*------------- sidebar ends -------------*/}
-    </div>
 
   );
 }
