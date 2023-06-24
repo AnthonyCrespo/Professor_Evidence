b83882ef1ac44fd7a9d5b2e50b0bfeba8b6ac3cb
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-06-24 11:52:30 -0500

    Added CRUD operations with API

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,7 +1,16 @@
+import { useNavigate} from "react-router-dom"
+
+
 export function TaskCard({task}){
+    
+    const navigate = useNavigate() 
     return(
-        <div>
-            <h1>{task.title}</h1>
+        <div  style ={{background: "black"}}
+            onClick={() => {
+                navigate("/tasks/"+task.id,)
+        }}
+        >
+            <h1>{task.tittle}</h1>
             <p>{task.description}</p>
             <hr/>
         </div>
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,12 +1,77 @@
-export function TaskFormPage(){
-    return (
-        <div>
-            <form action="">
-                <input type="text" placeholder="tittle" />
-                <textarea rows="3" placeholder="Description"></textarea>
-                <button>Save</button>
-            </form>
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { createTask, deleteTask, updateTask, getTask } from '../api/task.api';
+import { useNavigate, useParams} from 'react-router-dom';
+export function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue
+  } = useForm();
 
-        </div>
-    );
+ const navigate = useNavigate();
+
+    const params = useParams()
+    console.log(params)
+
+  /* Create or delete */
+  const onSubmit = handleSubmit(async (data) => {
+    if (params.id){
+        await updateTask(params.id,data)
+    } else {
+        await createTask(data);
+    }
+
+    navigate("/tasks")
+  });
+
+
+  /* Edit a task */
+  useEffect(()=>{
+        async function loadTask(){
+            if (params.id){
+                const res = await getTask(params.id)
+                setValue('tittle', res.data.tittle)
+                setValue('description', res.data.description)
+                //console.log(res)
+        }
+    }
+    loadTask()
+  },[])
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="tittle"
+          {...register('tittle', { required: true })}
+        />
+        {errors.tittle && <span>Title is required.</span>}
+
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register('description', { required: true })}
+        ></textarea>
+        {errors.description && <span>Description is required.</span>}
+
+        <button>Save</button>
+      </form>
+    {/* Delete a task */}
+    {params.id  &&
+          <button onClick={async ()=> {
+            const accepted = window.confirm("Are you sure")
+
+            if (accepted){
+                await deleteTask(params.id)
+                navigate("/tasks")
+            }
+          }}>
+          Delete
+        </button>}
+
+    </div>
+  );
 }
