ab83dda37790cf4663b9d9049f00157f9f04cc88
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-06-24 01:49:06 -0500

    First commit

---
+++ client/src/pages/TaskFormPage.jsx
@@ -0,0 +1,12 @@
+export function TaskFormPage(){
+    return (
+        <div>
+            <form action="">
+                <input type="text" placeholder="tittle" />
+                <textarea rows="3" placeholder="Description"></textarea>
+                <button>Save</button>
+            </form>
+
+        </div>
+    );
+}
