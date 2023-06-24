f0e2a11941e4e8dbf3ec9ebc98da28ed77c4fa52
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-06-24 17:55:41 -0500

    Commented code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,91 +19,93 @@ export function HomePage(){
 
         <div>
 
-        {/* navbar  */}
-        <Navbar className="SAED-navbar">
-          <Container>
-            <Navbar.Brand href="/home">
-                <img
-                src="/logo.png"
-                alt="logo"
-                width={230}
-                />  
-            </Navbar.Brand>
-{/*             <Nav className="ms-auto">
-              <Nav.Link  className="navbar-text" href="#home"> USUARIO APELLIDO</Nav.Link>
-            </Nav> */}
+            {/* ------------  navbar -------------  */}
+            <Navbar className="SAED-navbar">
+            <Container>
+                <Navbar.Brand /* href="/home" */>
+                    <img
+                    src="/logo.png"
+                    alt="logo"
+                    width={230}
+                    />  
+                </Navbar.Brand>
+            {/* <Nav className="ms-auto">
+                <Nav.Link  className="navbar-text" href="#home"> USUARIO APELLIDO</Nav.Link>
+                </Nav> */}
 
-          <NavDropdown title="NOMBRE APELLIDO" id="evidencias-dropdown">
-                <NavDropdown.Item href="/login"> Cerrar Sesion</NavDropdown.Item>
-          </NavDropdown>
-          </Container>
-        </Navbar>
+            <NavDropdown title="NOMBRE APELLIDO" id="evidencias-dropdown">
+                    <NavDropdown.Item href="/login"> Cerrar Sesion</NavDropdown.Item>
+            </NavDropdown>
+            </Container>
+            </Navbar>
+
+            {/* -----------  navbar end -------------  */}
 
 
 
-        {/* sidebar */}
-    <div className='row'>
-      <div className="col-auto min-vh-100" style={{backgroundColor:"RGB(39, 39, 39)"}}>
-        <ul className="nav flex-column mt-3">
+        {/*------------- sidebar -------------*/}
+        <div className='row'>
+            <div className="col-auto min-vh-100" style={{backgroundColor:"RGB(39, 39, 39)"}}>
+                <ul className="nav flex-column mt-3">
 
-        {/* -------Home option ------- */}
-          <li className="nav-item mt-2">
-            <a className="nav-link px-2" href="/">
-              <i className='bi bi-house side-bar-icon'></i>
-              <span className='ms-1 d-none d-sm-inline  side-bar-text'>
-                Home
-              </span>
-            </a>
-          </li>
-        
-        {/* -------Evidencias option ------- */}
+                {/* -------Home option ------- */}
+                <li className="nav-item mt-2">
+                    <a className="nav-link px-2" href="/home">
+                    <i className='bi bi-house side-bar-icon'></i>
+                    <span className='ms-1 d-none d-sm-inline  side-bar-text'>
+                        Home
+                    </span>
+                    </a>
+                </li>
+                
+                {/* -------Evidencias option ------- */}
 
-          <li className="nav-item mt-2">
-            <a className="nav-link px-2">
-              <i className='bi bi-file-text side-bar-icon '></i>
-              <span className='ms-1 d-none d-sm-inline side-bar-text'>
-                Evidencias
-              </span>
-            </a>
-            <ul className="nav flex-column ml-3">
-              <li className="nav-item">
-                <a className="nav-link" href="/evidencia1" style={{color:"white"}}>
-                  Registrar Evidencia
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="/evidencia2" style={{color:"white"}}>
-                  Evidencia Registrada
-                </a>
-              </li>
-            </ul>
-          </li>
+                <li className="nav-item mt-2">
+                    <a className="nav-link px-2">
+                    <i className='bi bi-file-text side-bar-icon '></i>
+                    <span className='ms-1 d-none d-sm-inline side-bar-text'>
+                        Evidencias
+                    </span>
+                    </a>
+                    <ul className="nav flex-column ml-3">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/evidencia1" style={{color:"white"}}>
+                        Registrar Evidencia
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link " href="/evidencia2" style={{color:"white"}}>
+                        Evidencia Registrada
+                        </a>
+                    </li>
+                    </ul>
+                </li>
 
-        {/* -------Informes option ------- */}
-          <li className="nav-item mt-2">
-            <a className="nav-link px-2" href="/">
-              <i className='bi bi-file-richtext side-bar-icon'></i>
-              <span className='ms-1 d-none d-sm-inline  side-bar-text'>
-                Informes
-              </span>
-            </a>
-            <ul className="nav flex-column ml-3">
-              <li className="nav-item  side-bar-text">
-                <a className="nav-link" href="/informe1" style={{color:"white"}}>
-                  Crear Informe
-                </a>
-              </li>
-              <li className="nav-item  side-bar-text">
-                <a className="nav-link" href="/informe2" style={{color:"white"}}>
-                  Observar Informe
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-
+                {/* -------Informes option ------- */}
+                <li className="nav-item mt-2">
+                    <a className="nav-link px-2" href="/">
+                    <i className='bi bi-file-richtext side-bar-icon'></i>
+                    <span className='ms-1 d-none d-sm-inline  side-bar-text'>
+                        Informes
+                    </span>
+                    </a>
+                    <ul className="nav flex-column ml-3">
+                    <li className="nav-item  side-bar-text">
+                        <a className="nav-link" href="/informe1" style={{color:"white"}}>
+                        Crear Informe
+                        </a>
+                    </li>
+                    <li className="nav-item  side-bar-text">
+                        <a className="nav-link" href="/informe2" style={{color:"white"}}>
+                        Observar Informe
+                        </a>
+                    </li>
+                    </ul>
+                </li>
+                </ul>
+            </div>
+        </div>
+        {/*------------- sidebar ends -------------*/}
     </div>
 
   );
