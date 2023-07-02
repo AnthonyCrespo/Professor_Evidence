2599395db481a788013a3ddc63c25a6d9a62977f
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-07-01 22:01:01 -0500

    Added sections for create report jsc

--- client/src/pages/Create_report.jsx
+++ client/src/pages/Create_report.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Base} from './Base';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import './css/Create_report.css';
 
 export function Create_report() {
   const renderFields = (category) => {
     return (
       <>
+        <h2>{category}</h2>
         <Form.Group className="mt-4">
           <Form.Label>Resumen:</Form.Label>
           <Form.Control as="textarea" rows={3} />
@@ -17,8 +19,8 @@ export function Create_report() {
           <Form.Control type="number" />
         </Form.Group>
 
-        <Form.Group className="mt-4">
-          <Form.Label>Número promedio de horas por semana dedicadas en el interseemestre:</Form.Label>
+        <Form.Group className="mt-4 mb-4">
+          <Form.Label>Número promedio de horas por semana dedicadas en el intersemestre:</Form.Label>
           <Form.Control type="number" />
         </Form.Group>
       </>
@@ -31,7 +33,7 @@ export function Create_report() {
 
       
         <Form className="w-50">
-          <Form.Group className="mt-4">
+{/*           <Form.Group className="mt-4">
             <Form.Label>Tipo de actividad:</Form.Label>
             <Form.Select>
               <option value="docencia">Docencia</option>
@@ -39,10 +41,12 @@ export function Create_report() {
               <option value="vinculacion">Vinculación con la sociedad</option>
               <option value="gestion">Gestión educativa</option>
             </Form.Select>
-          </Form.Group>
+          </Form.Group> */}
 
-          {renderFields('docencia')} {/* Cambiar 'docencia' por la categoría seleccionada */}
-
+          {renderFields('Docencia')}
+          {renderFields('Investigacion')} 
+          {renderFields('Vinculación con la Sociedad')}
+          {renderFields('Gestión Educativa')} 
           <Button className="mt-4" variant="primary" type="submit">Enviar</Button>
         </Form>
 
